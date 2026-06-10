7e2ed22d0e1704e4f6b3a6c0656b7274d675345b
Pedro Henrique Pedro <pedrohvs212@gmail.com>
2026-06-09 22:48:13 -0300

    melhorias e mais melhorias

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Alert, StyleSheet, Text, View, TextInput, Button, Image } from 'react-native';
+import { Alert, StyleSheet, Text, View, TextInput, Button, Image, TouchableOpacity } from 'react-native';
 import { ScrollView } from 'react-native-web';
 
 export default function BuscaPokemon() {
@@ -31,32 +31,35 @@ export default function BuscaPokemon() {
     <View style={styles.container}>
       <ScrollView contentContainerStyle={{ alignItems: 'center', paddingBottom: 20 }}>
         <Image
-          source={require('./assets/icon.png')}
-          style={{ width: 200, height: 80, marginTop: 20 }}
+          source={require('./assets/adaptive-icon-dark.png')}
+          style={{ width: 150, height: 120, marginTop: 20,  }}
         />
         <Text style={styles.titulo}>Pokedex</Text>
 
       <TextInput
         style={styles.input}
-        placeholder="Digite um Pokemon"
+        placeholder="Digite o nome ou ID do Pokémon"
         value={pesquisa}
         onChangeText={setPesquisa}
       />
 
-      <Button style={styles.buscar} title="Buscar" onPress={buscarPokemon} />
+      <TouchableOpacity style={styles.buscar} onPress={buscarPokemon}>
+        <Text style={styles.textoBotao}>Buscar</Text>
+      </TouchableOpacity>
 
       {pokemon && (
         <View>
           <Image
-            source={{ uri: pokemon.sprites.front_default }}
+            source={{ uri: pokemon.sprites.other['showdown'].front_default }}
             style={{
-              width: 300,
-              height: 220,
+              width: 340,
+              height: 340,
               marginBottom: 10,
               margin: 12,
               borderColor: '#FFFFFF',
               borderWidth: 2,
               borderRadius: 12,
+              backgroundColor: '#FFFFFF',
             }}
           />
 
@@ -65,9 +68,87 @@ export default function BuscaPokemon() {
             {pokemon.types.map(t => t.type.name).join(', ')}
           </Text>
           <Text style={styles.habilidades}>{pokemon.abilities.map(a => a.ability.name).join(', ')}</Text>
+          
+          <Text style={styles.titulo}>• Stats</Text>
+
           <Text style={styles.stats}>
-            {pokemon.stats.map(s => `${s.stat.name}: ${s.base_stat}`).join('\n')}
+            Experiencia base: {pokemon.base_experience}
           </Text>
+          <Text style={styles.stats}>
+            ❤ HP: {pokemon.stats.find(s => s.stat.name === 'hp').base_stat}
+          </Text>
+          <Text style={styles.stats}>
+            🗡 Ataque: {pokemon.stats.find(s => s.stat.name === 'attack').base_stat}
+          </Text>
+            <Text style={styles.stats}>
+            🛡 Defesa: {pokemon.stats.find(s => s.stat.name === 'defense').base_stat}
+          </Text>
+          <Text style={styles.stats}>
+            ⚔ Ataque-Especial: {pokemon.stats.find(s => s.stat.name === 'special-attack').base_stat}
+          </Text>
+          <Text style={styles.stats}>
+            🛡 Defesa-Especial: {pokemon.stats.find(s => s.stat.name === 'special-defense').base_stat}
+          </Text>
+          <Text style={styles.stats}>
+            🏃‍♂️ Velocidade: {pokemon.stats.find(s => s.stat.name === 'speed').base_stat}
+          </Text>
+            <Text style={styles.stats}>
+            ⚖ Peso: {pokemon.weight} hg
+          </Text>
+          <Text style={styles.stats}>
+            📏 Altura: {pokemon.height} dm
+          </Text>
+
+            <Text style={styles.titulo}>• Sprites</Text>
+
+            <Text style={styles.subtitulo}>Normal</Text>
+
+            <Image
+            source={{ uri: pokemon.sprites.other['official-artwork'].front_default }}
+            style={{
+              width: 300,
+              height: 300,
+              marginBottom: 10,
+              margin: 12,
+              borderColor: '#FFFFFF',
+              borderWidth: 2,
+              borderRadius: 12,
+              backgroundColor: '#FFFFFF',
+            }}
+          />
+
+            <Text style={styles.subtitulo}>Shiny</Text>
+            <Image
+            source={{ uri: pokemon.sprites.other['official-artwork'].front_shiny }}
+            style={{
+              width: 300,
+              height: 300,
+              marginBottom: 10,
+              margin: 12,
+              borderColor: '#FFFFFF',
+              borderWidth: 2,
+              borderRadius: 12,
+              backgroundColor: '#FFFFFF',
+            }}
+          />
+
+          <Text style={styles.subtitulo}>3D</Text>
+          <Image
+            source={{ uri: pokemon.sprites.other['home'].front_default }}
+            style={{
+              width: 300,
+              height: 300,
+              marginBottom: 10,
+              margin: 12,
+              borderColor: '#FFFFFF',
+              borderWidth: 2,
+              borderRadius: 12,
+              backgroundColor: '#FFFFFF',
+            }}
+          />
+
+          
+          
         </View>
       )}
 
@@ -83,24 +164,49 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "flex-start",
-    backgroundColor: '#262626',
+    backgroundColor: '#0e0e0e',
   },
 
   titulo: {
-    marginTop: 50,
+    marginTop: 10,
+    fontSize: 40,
+    marginBottom: 20,
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+  },
+  subtitulo: {
+    marginTop: 10,
     fontSize: 30,
     marginBottom: 20,
     color: '#FFFFFF',
+    fontWeight: 'bold',
   },
   input: {
     width: '80%',
-    height: 50,
+    height: 80,
     borderColor: '#ccc',
     borderWidth: 1,
     marginBottom: 10,
     padding: 20,
     color: '#FFFFFF',
     borderRadius: 8,
+    fontSize: 18,
+    backgroundColor: '#1e1e1e',
+  },
+  buscar: {
+    marginBottom: 20,
+    backgroundColor: '#1c65ec',
+    padding: 10,
+    borderRadius: 5,
+    fontSize: 18,
+    width: '80%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textoBotao: {
+    color: '#FFFFFF',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
   nome: {
     fontSize: 26,
@@ -129,5 +235,14 @@ const styles = StyleSheet.create({
   stats: {
     fontSize: 18,
     color: '#FFFFFF',
+  },
+  medidas: {
+    fontSize: 16,
+    color: '#FFFFFF',
+    backgroundColor: '#A3A3A3',
+    padding: 5,
+    borderRadius: 5,
+    marginTop: 5,
+    textAlign: 'center',
   },
 });
